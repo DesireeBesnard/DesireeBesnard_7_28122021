c0e73cc7d5200f5b03d5beb322137eac172a7d71
DesireeBesnard <desiree.besnard@gmail.com>
2022-01-20 09:34:34 +0100

    main search function

--- js/script.js
+++ js/script.js
@@ -1,137 +1,143 @@
-// const input = "Délice de citron à l'oignon au, gruyère et dés de lait de coco c'est lourd d'des"
 const input = "coco"
 const searchValue = input.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().split(' ')
-// console.log(searchValue)
 
-const toDelete = Object.entries({
-    "apostrophe": ["c'", "d'", "j'", "l'", "m'", "n'", "t'"],
-    "punctuation": [",", ";", ":"],
-    "articles": ['a', 'au', 'aux', 'avec', 'dans', 'de', 'des', 'du', 'en', 'et', 'est', 'la', 'le', 'les', 'par', 'pour', 'sans', 'sous', 'sur', 'un', 'une']
-})
-let wToSearch = []
+const mainSearch = searchValue => {
 
-let results = []
+    let wToSearch = []
+    let results = []
+    const toDelete = Object.entries({
+        "apostrophe": ["c'", "d'", "j'", "l'", "m'", "n'", "t'"],
+        "punctuation": [",", ";", ":"],
+        "articles": ['a', 'au', 'aux', 'avec', 'dans', 'de', 'des', 'du', 'en', 'et', 'est', 'la', 'le', 'les', 'par', 'pour', 'sans', 'sous', 'sur', 'un', 'une']
+    })
 
-const inArray = (array, word) => {
-    for (let i = 0; i < array.length; i++) {
-        const searchWord = array[i]
-        if (searchWord === word) {
-            return true
+    const inArray = (array, word) => {
+        for (let i = 0; i < array.length; i++) {
+            const searchWord = array[i]
+            if (searchWord === word) {
+                return true
+            } 
+        }
+        return false
+    }
+    
+    const gotApostrophe = word => {
+        for (let i = 0; i < toDelete[0][1].length; i++) {
+            const apostrophe = toDelete[0][1][i];
+            const regex = new RegExp(apostrophe);
+            if (regex.test(word) === true) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    const gotPunct = word => {
+        for (let i = 0; i < toDelete[1][1].length; i++) {
+            const wToDelete = toDelete[1][1][i];
+            if (inArray(word, wToDelete) === true) {
+                return true
+            }  
+        }
+        return false
+    }
+    
+    // Remove all irrelevant words
+    for (let index = 0; index < toDelete.length; index++) {
+        let category = toDelete[index]
+        let items = category[1]
+    
+        if (category[0] === "apostrophe") {
+            for (let i = 0; i < items.length; i++) {
+                const wToDelete = items[i]
+    
+                for (let j = 0; j < searchValue.length; j++) {
+                    let word = searchValue[j]
+    
+                    if (inArray(word, "'") === true) {
+                        for (let i = 0; i < toDelete[0][1].length; i++) {
+    
+                            const toModify = new RegExp(wToDelete)
+    
+                            if (toModify.test(word) === true) {
+                                const modifWord = word.replace(wToDelete, "")
+                                searchValue[j] = modifWord
+                            }
+                        }
+                    }
+                }
+    
+            }
+        } else if (category[0] === "punctuation") {
+            for (let i = 0; i < items.length; i++) {
+                const wToDelete = items[i]
+    
+                for (let j = 0; j < searchValue.length; j++) {
+                    let word = searchValue[j]
+    
+                    if (gotPunct(word) === true) {
+                        for (let i = 0; i < toDelete[1][1].length; i++) {
+    
+                            const toModify = new RegExp(wToDelete)
+    
+                            if (toModify.test(word) === true) {
+                                const modifWord = word.replace(wToDelete, "")
+    
+                                searchValue[j] = modifWord
+                            }
+                        }
+                    }
+                }
+            }
+        } else if (category[0] === "articles") {
+            for (let i = 0; i < items.length; i++) {
+                const wToDelete = items[i]
+                
+                for (let j = 0; j < searchValue.length; j++) {
+                    let word = searchValue[j]
+              
+                    if ((inArray(toDelete[2][1], word) === false) && (inArray(wToSearch, word) === false) ) {
+                        wToSearch[wToSearch.length] = word
+                    }
+                }
+            }
         } 
     }
-    return false
-}
 
-const gotApostrophe = word => {
-    for (let i = 0; i < toDelete[0][1].length; i++) {
-        const apostrophe = toDelete[0][1][i];
-        const regex = new RegExp(apostrophe);
-        if (regex.test(word) === true) {
-            return true
-        }
-    }
-    return false
-}
-
-const gotPunct = word => {
-    for (let i = 0; i < toDelete[1][1].length; i++) {
-        const wToDelete = toDelete[1][1][i];
-        if (inArray(word, wToDelete) === true) {
-            return true
-        }  
-    }
-    return false
-}
-
-// Remove all irrelevant words
-for (let index = 0; index < toDelete.length; index++) {
-    let category = toDelete[index]
-    let items = category[1]
-
-    if (category[0] === "apostrophe") {
-        for (let i = 0; i < items.length; i++) {
-            const wToDelete = items[i]
-
-            for (let j = 0; j < searchValue.length; j++) {
-                let word = searchValue[j]
-
-                if (inArray(word, "'") === true) {
-                    for (let i = 0; i < toDelete[0][1].length; i++) {
-
-                        const toModify = new RegExp(wToDelete)
-
-                        if (toModify.test(word) === true) {
-                            const modifWord = word.replace(wToDelete, "")
-                            searchValue[j] = modifWord
-                        }
-                    }
+    const findResults = (value) => {
+        for (const recipe of recipes) {
+    
+            const regex = new RegExp(value)
+    
+            if ( (regex.test(recipe.name.toLowerCase()) === true) && (inArray(results, recipe) === false) ) {
+                results[results.length] = recipe
+            }
+    
+            for (let i = 0; i < recipe.ingredients.length; i++) {
+                const ingredient = recipe.ingredients[i].ingredient.toLowerCase()
+    
+                if ( (regex.test(ingredient) === true) && (inArray(results, recipe) === false)) {
+                    results[results.length] = recipe
                 }
             }
-
-        }
-    } else if (category[0] === "punctuation") {
-        for (let i = 0; i < items.length; i++) {
-            const wToDelete = items[i]
-
-            for (let j = 0; j < searchValue.length; j++) {
-                let word = searchValue[j]
-
-                if (gotPunct(word) === true) {
-                    for (let i = 0; i < toDelete[1][1].length; i++) {
-
-                        const toModify = new RegExp(wToDelete)
-
-                        if (toModify.test(word) === true) {
-                            const modifWord = word.replace(wToDelete, "")
-
-                            searchValue[j] = modifWord
-                        }
-                    }
-                }
-            }
-        }
-    } else if (category[0] === "articles") {
-        for (let i = 0; i < items.length; i++) {
-            const wToDelete = items[i]
-            
-            for (let j = 0; j < searchValue.length; j++) {
-                let word = searchValue[j]
-          
-                if ((inArray(toDelete[2][1], word) === false) && (inArray(wToSearch, word) === false) ) {
-                    wToSearch[wToSearch.length] = word
-                }
-            }
-        }
-    } 
-}
-
-const findResults = (value) => {
-    for (const recipe of recipes) {
-
-        const regex = new RegExp(value)
-
-        if ( (regex.test(recipe.name.toLowerCase()) === true) && (inArray(results, recipe) === false) ) {
-            results[results.length] = recipe
-        }
-
-        for (let i = 0; i < recipe.ingredients.length; i++) {
-            const ingredient = recipe.ingredients[i].ingredient.toLowerCase()
-
-            if ( (regex.test(ingredient) === true) && (inArray(results, recipe) === false)) {
+    
+            if ( (regex.test(recipe.description.toLowerCase()) === true) && (inArray(results, recipe) === false)) {
                 results[results.length] = recipe
             }
         }
-
-        if ( (regex.test(recipe.description.toLowerCase()) === true) && (inArray(results, recipe) === false)) {
-            results[results.length] = recipe
-        }
     }
+    
+    for (let i = 0; i < wToSearch.length; i++) {
+        const word = wToSearch[i]
+    
+        findResults(word)
+    }
+
+    console.log(results)
 }
 
-for (let i = 0; i < wToSearch.length; i++) {
-    const word = wToSearch[i]
+mainSearch(searchValue)
 
-    findResults(word)
-}
 
-console.log(results)
+
+
